e34b1ea4327625138261259314f1e2403f0c38a7
Andrii Sidorov <ss60600@gmail.com>
2021-06-13 23:55:25 +0300

    Add logic

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var cards = document.querySelectorAll('.flip-card');
 cards.forEach(card => {
     card.addEventListener( 'click', function() {
+      if(card.classList.contains('is-flipped') === false) {
+        var answer = prompt('Your answer is ...', "");
+        
+          if (answer.toLowerCase() === 'hulk' || answer.toLowerCase() === 'dumbledore') {
+            card.classList.toggle('is-flipped');
+          } else {
+            card.classList.toggle('invalid');
+          }
+      } else {
         card.classList.toggle('is-flipped');
-      });
+      }
+    });
 });
